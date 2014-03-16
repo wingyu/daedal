10683703695f15c185bfa9c7fedd442c448587a1
Christopher Schuch <chris@rallypoint.com>
2014-03-16 15:16:30 -0400

    Fixing boolean query values

--- lib/daedal/attributes/query_value.rb
+++ lib/daedal/attributes/query_value.rb
@@ -4,7 +4,7 @@ module Daedal
     a string, a symbol, a float, or an integer. If it's none
     of those, raises a coercion error."""
     class QueryValue < Virtus::Attribute
-      ALLOWED_QUERY_VALUE_CLASSES = [String, Symbol, Float, Fixnum, Boolean]
+      ALLOWED_QUERY_VALUE_CLASSES = [String, Symbol, Float, Fixnum, Boolean, TrueClass, FalseClass]
       def coerce(q)
         if !required? and q.nil?
           return q
